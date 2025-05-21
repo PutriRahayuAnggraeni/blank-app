1c6ce8f0e5444cab19f9a2093a3cd5b04d25ee8e
PutriRahayuAnggraeni <putrirahayuuy@gmail.com>
2025-05-21 14:24:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("welcome!")
 st.write(
     "come play with me ><."
 )
+st.image("haygainusa on Instagram_ Have you ever seen a horse swimming before_ We loved getting to see the horse pool in action and learn about the incredible benefits at….jpeg",width=200)
